dad28d491ef8e5865d38e8ec430ab925c2fd6115
yzwu <youzi.wu1101@gmail.com>
2015-05-10 02:28:15 -0400

    oops:

--- local/hp_xubuntu/.mozilla/firefox/a6sfcr35.default/extensions/FavIconReloader@mozilla.org/defaults/preferences/FavIconReloader.js
+++
@@ -1,8 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-pref("extensions.FavIconReloader.maxTasks", "10");
-pref("extensions.FavIconReloader.timeout", "15");
-pref("extensions.FavIconReloader.showDebugText", false);
